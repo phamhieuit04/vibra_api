e64b1612bde2665eeac131cb53534e4d2776f45c
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-05-13 00:31:59 +0700

    Add Laravel Ide Helper

---
+++ _ide_helper_models.php
@@ -0,0 +1,149 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int|null $playlist_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Bill newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Bill newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Bill query()
+ */
+	class Bill extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $bill_id
+ * @property int|null $song_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|BillDetail newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BillDetail newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BillDetail query()
+ */
+	class BillDetail extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int|null $song_id
+ * @property int|null $artist_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Blocked newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Blocked newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Blocked query()
+ */
+	class Blocked extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int|null $playlist_id
+ * @property int|null $artist_id
+ * @property int|null $song_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Library newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Library newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Library query()
+ */
+	class Library extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property int $author_id
+ * @property string $thumbnail
+ * @property int $type
+ * @property int $total_song
+ * @property int $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Playlist newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Playlist newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Playlist query()
+ */
+	class Playlist extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $author_id
+ * @property int|null $playlist_id
+ * @property string $lyrics
+ * @property string $thumbnail
+ * @property int $total_played
+ * @property int $status
+ * @property int $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Song newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Song newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Song query()
+ */
+	class Song extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $gender
+ * @property \Illuminate\Support\Carbon|null $birth
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string $avatar
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ */
+	class User extends \Eloquent {}
+}
+
