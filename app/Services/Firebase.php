3d0852c88ac85f8a8ac13927afeec9faf97d2035
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-05-14 21:17:23 +0700

    Add laravel firebase

---
+++ app/Services/Firebase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+
+class Firebase
+{
+	public static function FirebaseAuthentication(Request $request)
+	{
+		$params = $request->all();
+		// TODO: firebase authentication
+	}
+}
