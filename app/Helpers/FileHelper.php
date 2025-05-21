05217b4612932e2941b30dff044fcfdfd5c70633
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-05-21 23:13:29 +0700

    Update FileHelper.php

--- app/Helpers/FileHelper.php
+++ app/Helpers/FileHelper.php
@@ -2,50 +2,65 @@
 
 namespace App\Helpers;
 
+use App\Models\Song;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class FileHelper
 {
-	public static function getName($file)
+	private static function getNameFromEmail($user = null)
+	{
+		if (!is_null($user)) {
+			return explode('@', $user->email)[0];
+		} else {
+			return explode('@', Auth::user()->email)[0];
+		}
+	}
+
+	public static function getFileName($file)
 	{
 		$lastDot = strrpos($file->getClientOriginalName(), '.');
 		$name = substr($file->getClientOriginalName(), 0, $lastDot);
 		return $name;
 	}
 
-	public static function storeSong($file)
+	public static function store($file, string $location)
 	{
 		if (!is_null($file)) {
-			$cleanEmail = explode('@', Auth::user()->email)[0];
-			$fullPath = 'uploads/' . $cleanEmail . '/songs';
-			$file->storeAs($fullPath, $file->getClientOriginalName(), ['disk' => 'public-api']);
+			if ($location === 'songs' || $location === 'lyrics' || $location === 'thumbnails' || $location === 'avatars') {
+				$fullPath = 'uploads/' . self::getNameFromEmail() . '/' . $location;
+				$file->storeAs($fullPath, $file->getClientOriginalName(), ['disk' => 'public-api']);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static function getUrl(string $location, Song $song)
+	{
+		if (!is_null($song) && ($location === 'songs' || $location === 'lyrics' || $location === 'thumbnails')) {
+			$path = env('APP_URL') . '/uploads/' . self::getNameFromEmail() . '/' . $location . '/';
+			switch ($location) {
+				case 'songs':
+					return $path . $song->name . '.mp3';
+				case 'lyrics':
+					return $path . $song->lyrics;
+				case 'thumbnails':
+					return $path . $song->thumbnail;
+			}
+		} else {
+			return null;
 		}
 	}
 
-	public static function storeLyric($file)
+	public static function getAvatar(User $user = null)
 	{
-		if (!is_null($file)) {
-			$cleanEmail = explode('@', Auth::user()->email)[0];
-			$fullPath = 'uploads/' . $cleanEmail . '/lyrics';
-			$file->storeAs($fullPath, $file->getClientOriginalName(), ['disk' => 'public-api']);
+		if (!is_null($user)) {
+			return env('APP_URL') . '/uploads/' . self::getNameFromEmail($user) . '/avatars/' . $user->avatar;
+		} else {
+			return env('APP_URL') . '/uploads/' . self::getNameFromEmail() . '/avatars/' . Auth::user()->avatar;
 		}
 	}
 
-	public static function songPath($song)
-	{
-		if (!is_null($song)) {
-			$cleanEmail = explode('@', Auth::user()->email)[0];
-			$fullPath = env('APP_URL') . '/uploads/' . $cleanEmail . '/songs/' . $song->name . '.mp3';
-			return $fullPath;
-		}
-	}
-
-	public static function lyricPath($song)
-	{
-		if (!is_null($song)) {
-			$cleanEmail = explode('@', Auth::user()->email)[0];
-			$fullPath = env('APP_URL') . '/uploads/' . $cleanEmail . '/lyrics' . $song->lyrics;
-			return $fullPath;
-		}
-	}
+	// TODO: refactor all get url functions, fix missing thumbnail url for playlist and category
 }
