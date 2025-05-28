4d20d918ca4bb8ed5c7d55b2ededeb07889c6935
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-05-28 13:17:46 +0700

    Update PayOSService.php

--- app/Services/PayOSService.php
+++ app/Services/PayOSService.php
@@ -8,26 +8,26 @@ use function Laravel\Prompts\select;
 
 class PayOSService
 {
-    private $payOS;
-    public function __construct()
-    {
-        $this->payOS = new PayOS(env('CLIENT_ID'), env('API_KEY'), env('CHECKSUM_KEY'));
-    }
-    public static function createPaymentLink(Bill $bill)
-    {
-        $self = new self();
-        try {
-            $data = [
-                "orderCode" => $bill->id,
-                "amount" => $bill->total_price,
-                "description" => "Thanh toan hoa don " . $bill->id,
-                "returnUrl" => "http://localhost:5173/verify",
-                "cancelUrl" => "http://localhost:5173/verify"
-            ];
-            $result = $self->payOS->createPaymentLink($data);
-            return $result;
-        } catch (\Throwable $th) {
-            return null;
-        }
-    }
+	private $payOS;
+	public function __construct()
+	{
+		$this->payOS = new PayOS(env('CLIENT_ID'), env('API_KEY'), env('CHECKSUM_KEY'));
+	}
+	public static function createPaymentLink(Bill $bill)
+	{
+		$self = new self();
+		try {
+			$data = [
+				"orderCode" => $bill->id,
+				"amount" => $bill->total_price,
+				"description" => "Thanh toan hoa don " . $bill->id,
+				"returnUrl" => "http://localhost:5173/paysuccess",
+				"cancelUrl" => "http://localhost:5173/payfail"
+			];
+			$result = $self->payOS->createPaymentLink($data);
+			return $result;
+		} catch (\Throwable $th) {
+			return null;
+		}
+	}
 }
